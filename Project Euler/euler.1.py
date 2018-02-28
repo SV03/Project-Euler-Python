4224405941879abd9103e5a641d24a7f6e991e2e
Sarath <Sarath.Vaman@city.ac.uk>
2018-02-28 02:30:37 +0000

    update using list comphension and generator expression

--- Project Euler/euler.1.py
+++ Project Euler/euler.1.py
@@ -1,11 +1,8 @@
 def sum_of_div_35(to=10):
-
-	result = 0
-	for i in xrange(1,to):
-		 if i  % 3 == 0 or i % 5 == 0:
-		 	result += i
+	return sum((i for i in range(1, to) if div_b_35(i))
 
 
-	return result
+def div_by_35():
+	return x  % 3 == 0 or i % 5 == 0
 
-print sum_of_div_35()
+print sum_of_div_35(1000)
